630acf82240e017534e20045b80da07397fc87cf
Ryan <ryanzhutch@gmail.com>
2021-10-25 10:19:46 -0600

    added constants structure

--- API/Modules/App/Customer/SampleCustomers/Services/SampleCustomerService.cs
+++ API/Modules/App/Customer/SampleCustomers/Services/SampleCustomerService.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using API.Modules.Base.Services;
 using API.Modules.Base.Services.Mongo.MongoData;
+using API.MongoData;
 using API.MongoData.Model;
 using API.SampleCustomers.Interfaces;
 using MongoDB.Driver;
@@ -14,8 +15,8 @@ namespace API.Customers.SampleCustomers.Service
         public SampleCustomerService(IMongoSettings mongoSettings)
         {
             var client = new MongoClient(mongoSettings.MongoUri);
-            var database = client.GetDatabase("sample_analytics");
-            _sampleCustomer = database.GetCollection<SampleCustomer>("customers");
+            var database = client.GetDatabase(Documents.SampleCustomers.Database);
+            _sampleCustomer = database.GetCollection<SampleCustomer>(Documents.SampleCustomers.Collection);
         }
 
         public SampleCustomer Create(SampleCustomer sampleCustomer)
@@ -23,18 +24,26 @@ namespace API.Customers.SampleCustomers.Service
             _sampleCustomer.InsertOne(sampleCustomer);
             return sampleCustomer;
         }
-        
-        public SampleCustomer FindById(string mongoId) => 
-            _sampleCustomer.Find(x => x.MongoId == mongoId).SingleOrDefault();
-        
 
-        public IList<SampleCustomer> Read() =>
-            _sampleCustomer.Find(x => true).ToList();
+        public SampleCustomer FindById(string mongoId)
+        {
+            return _sampleCustomer.Find(x => x.MongoId == mongoId).SingleOrDefault();
+        }
 
-        public void Update(SampleCustomer sampleCustomer) =>
+
+        public IList<SampleCustomer> Read()
+        {
+            return _sampleCustomer.Find(x => true).ToList();
+        }
+
+        public void Update(SampleCustomer sampleCustomer)
+        {
             _sampleCustomer.ReplaceOne(x => x.MongoId == sampleCustomer.MongoId, sampleCustomer);
+        }
 
-        public void Delete(string mongoId) =>
+        public void Delete(string mongoId)
+        {
             _sampleCustomer.DeleteOne(x => x.MongoId == mongoId);
+        }
     }
 }
---
+++ API/Modules/Base/Services/Constants.cs
@@ -0,0 +1,11 @@
+namespace API.MongoData
+{
+  public struct Documents
+  {
+    public struct SampleCustomers
+    {
+      public const string Database = "sample_analytics";
+      public const string Collection = "customers";
+    }
+  }
+}
